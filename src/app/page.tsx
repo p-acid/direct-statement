04b7051177321dd53e67d78db0dc203230f8b783
p-acid <qkrtkstjd01@gmail.com>
2023-12-05 23:48:13 +0900

    style: 폼 마크업 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,25 @@
-import { Separator } from "@/components/ui/separator"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 import Form from "./src/ui/form"
-import ThemeButtons from "./src/ui/theme-buttons"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-10 bg-zinc-50 transition-colors dark:bg-zinc-900">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-5xl font-bold tracking-tight text-zinc-800 transition-colors dark:text-zinc-300">
-          이지택스
-        </h1>
-
-        <p className="text-xl font-light tracking-tight text-zinc-800 transition-colors dark:text-zinc-200">
-          세금명세서를 더 간편히 작성하세요
-        </p>
-      </div>
-
-      <Separator />
-
-      <Form />
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 bg-zinc-50 transition-colors dark:bg-black">
+      <Card>
+        <CardHeader>
+          <CardTitle>이지택스</CardTitle>
+          <CardDescription>세금명세서를 더 간편히 작성하세요</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form />
+        </CardContent>
+      </Card>
     </main>
   )
 }
--- src/app/src/ui/form.tsx
+++ src/app/src/ui/form.tsx
@@ -1,25 +1,81 @@
 "use client"
 
-import { FormEvent, useRef } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-const Form = () => {
-  const inputRef = useRef<HTMLInputElement>(null)
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "최소 한 글자를 입력해주세요" })
+    .email("올바른 이메일 형태가 아닙니다."),
+  password: z.string().min(1, { message: "최소 한 글자를 입력해주세요" }),
+})
 
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    alert(inputRef?.current?.value)
+const SignInForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
   }
 
   return (
-    <form className="flex w-full flex-col gap-3" onSubmit={onSubmit}>
-      <Input ref={inputRef} placeholder="Add keyword" />
+    <Form {...form}>
+      <form
+        className="flex w-[400px] flex-col gap-3"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>이메일</FormLabel>
+              <FormControl>
+                <Input placeholder="아이디를 입력하세요." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <Button type="submit">로그인</Button>
-    </form>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호</FormLabel>
+              <FormControl>
+                <Input placeholder="비밀번호를 입력하세요." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="mt-2" type="submit">
+          로그인
+        </Button>
+      </form>
+    </Form>
   )
 }
 
-export default Form
+export default SignInForm
